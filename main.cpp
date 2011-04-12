8e3d6b228f9a119e36de5da94f5f9892d9df918b
Son Tung Nguyen <stefan@vinova.sg>
2011-04-13 01:45:12 +0700

    init Node class

--- main.cpp
+++ main.cpp
@@ -18,6 +18,12 @@
 
 using namespace std;
 
+/*
+ * =====================================================================================
+ *        Class:  State
+ *  Description:  A game state
+ * =====================================================================================
+ */
 class State {
   private:
     vector<int> m_Tiles;
@@ -35,6 +41,34 @@ class State {
         // std::vector can be deeply compare by using ==
         return this->m_Tiles == state->m_Tiles;
       }		/* -----  end of function IsSameState  ----- */
+};
+
+/*
+ * =====================================================================================
+ *        Class:  Node
+ *  Description:  A node in the search tree.
+ *                Because this program does not return the solution path (if found),
+ *                components such as Action, Parent Node are ommited to save memory.
+ * =====================================================================================
+ */
+class Node {
+  private:
+    State* m_State;
+    int    m_PathCost;  // also is the depth of the node
+
+  public:
+
+    /* 
+     * ===  FUNCTION  ======================================================================
+     *         Name:  GenerateSuccessors
+     *  Description:  
+     * =====================================================================================
+     */
+    vector<Node>*
+      GenerateSuccessors ()
+      {
+        vector<Node>* successors;
+      }		/* -----  end of function GenerateSuccessors  ----- */
 };
 
 int main()
