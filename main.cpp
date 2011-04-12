06ae09a601ee196f3a6de89576375d609b4a14a3
Son Tung Nguyen <stefan@vinova.sg>
2011-04-13 01:32:58 +0700

    State class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Solve n-Puzzle using A* search algorithm
+ *                  Assignment 01 - course AI, UET, VNU
+ *
+ *        Created:  04/13/2011 01:10:36 AM
+ *
+ *         Author:  Nguyen Son Tung (sontung23@gmail.com)
+ *
+ * =====================================================================================
+ */
+
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+class State {
+  private:
+    vector<int> m_Tiles;
+
+  public:
+    /* 
+     * ===  FUNCTION  ======================================================================
+     *         Name:  IsSameState
+     *  Description:  Check to see if the passed in state is the same as this state
+     * =====================================================================================
+     */
+    bool
+      IsSameState ( State* state )
+      {
+        // std::vector can be deeply compare by using ==
+        return this->m_Tiles == state->m_Tiles;
+      }		/* -----  end of function IsSameState  ----- */
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
     return 0;
 }
